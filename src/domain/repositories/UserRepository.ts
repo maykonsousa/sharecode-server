e1137b21002bd6d30bbe49a34fbe2469a033ee4e
Juliano Silva <cont.juliano@outlook.com>
2022-11-29 19:39:55 -0300

    fix: fixed to 4 spaces

--- src/domain/repositories/UserRepository.ts
+++ src/domain/repositories/UserRepository.ts
@@ -1,11 +1,11 @@
 import { User } from '../../domain/entities/User'
 
 export interface UserRepository {
-  save(User: User): Promise<void>
-  find(id: string): Promise<User>
-  findByEmail(email: string): Promise<User>
-  findAll(): Promise<User[]>
-  update(User: User): Promise<void>
-  delete(id: string): Promise<void>
-  clean(): Promise<void>
+    save(User: User): Promise<void>
+    find(id: string): Promise<User>
+    findByEmail(email: string): Promise<User>
+    findAll(): Promise<User[]>
+    update(User: User): Promise<void>
+    delete(id: string): Promise<void>
+    clean(): Promise<void>
 }
