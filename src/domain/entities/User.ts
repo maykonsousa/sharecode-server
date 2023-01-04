192f4394ae3bc394afba91ae0b4ef96a30105ca4
Juliano Silva <cont.juliano@outlook.com>
2023-01-04 08:36:11 -0300

    feat: added a validation to create user

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -11,9 +11,16 @@ export class User {
         readonly name: string,
         email: string,
         password: string,
-        readonly type: string
+        readonly type: string = 'user'
     ) {
+        this.validate()
         this.email = new Email(email)
         this.password = new Password(password)
+    }
+
+    private validate() {
+        if (!this.id) throw new Error('id is required')
+        if (!this.gh_username) throw new Error('gh_username is required')
+        if (!this.name) throw new Error('name is required')
     }
 }
