301b351d9cf88b05745cbebd4f2a2aeae8c96193
Juliano Silva <cont.juliano@outlook.com>
2023-05-28 11:28:31 -0300

    refactor: refactored user entity

--- src/core/domain/User.ts
+++ src/core/domain/User.ts
@@ -3,21 +3,16 @@ import { Password } from './value-objects/Password'
 import { Rule } from './value-objects/Rule'
 
 export class User {
-    email: Email
-    password: Password
-    private rule: string
 
-    constructor(
+    private constructor(
         readonly id: string,
         readonly gh_username: string,
         readonly name: string,
-        email: string,
-        password: string,
+        readonly email: string,
+        private password: string,
+        private rule: string
     ) {
         this.validate()
-        this.email = new Email(email)
-        this.password = new Password(password)
-        this.rule = new Rule('user').getValue()
     }
 
     private validate() {
@@ -26,11 +21,60 @@ export class User {
         if (!this.name) throw new Error('name is required')
     }
 
+    static create(
+        id: string,
+        gh_username: string,
+        name: string,
+        email: string,
+        password: string
+    ): User {
+        return new User(
+            id,
+            gh_username,
+            name,
+            new Email(email).getValue(),
+            new Password(password).getValue(),
+            new Rule('user').getValue()
+        )
+    }
+
+    static buildExistingUser(
+        id: string,
+        gh_username: string,
+        name: string,
+        email: string,
+        password: string,
+        rule: string
+    ): User {
+        return new User(
+            id,
+            gh_username,
+            name,
+            email,
+            password,
+            rule
+        )
+    }
+
+ 
+
     updateRule(rule: string): void {
         this.rule = new Rule(rule).getValue()
     }
 
     getRule(): string {
         return this.rule
+    }
+
+    updatePassword(password: string): void {
+        this.password = new Password(password).getValue()
+    }
+
+    updateEncrypedPassword(password: string): void {
+        this.password = password
+    }
+
+    getPassword(): string {
+        return this.password
     }
 }
