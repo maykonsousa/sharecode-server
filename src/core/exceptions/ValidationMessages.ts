3766a022f3918ff71a871113eafebcc4ef83fecd
Juliano Silva <cont.juliano@outlook.com>
2023-05-28 12:18:29 -0300

    feat: added validation messages

---
+++ src/core/exceptions/ValidationMessages.ts
@@ -0,0 +1,8 @@
+export class ValidationMessages {
+    public static readonly EMPTY_USER_ID = 'user id cannot be empty'
+    public static readonly EMPTY_USER_NAME = 'user name cannot be empty'
+    public static readonly EMPTY_USER_USERNAME = 'user username cannot be empty'
+    public static readonly INVALID_EMAIL = 'invalid email'
+    public static readonly INVALID_PASSWORD = 'invalid password'
+    public static readonly INVALID_RULE = 'invalid rule'
+}
