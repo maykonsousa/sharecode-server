9f8aa5a1a6d4019da451e21c1069385ac12320e3
Juliano Silva <cont.juliano@outlook.com>
2023-06-17 12:28:45 -0300

    feat: added defaults

---
+++ src/core/domain/defaults/UserDefaults.ts
@@ -0,0 +1,7 @@
+export class UserDefaults {
+    public static readonly DEFAULT_USER_ID = '1'
+    public static readonly DEFAULT_USER_NAME = 'John Doe'
+    public static readonly DEFAULT_USER_USERNAME = 'jhondoe'
+    public static readonly DEFAULT_USER_EMAIL = 'jhon.doe@test.com'
+    public static readonly DEFAULT_USER_PASSWORD = 'P4ssw04d@'
+}
