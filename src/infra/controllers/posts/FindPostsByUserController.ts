ce5e98f2dad4e1b24c46c585afa4a846f151c4d2
Juliano Silva <cont.juliano@outlook.com>
2023-07-16 10:29:33 -0300

    refactor: refactored custom request

--- src/infra/controllers/posts/FindPostsByUserController.ts
+++ src/infra/controllers/posts/FindPostsByUserController.ts
@@ -1,16 +1,17 @@
-import { NextFunction, Request, Response } from 'express'
+import { NextFunction, Response } from 'express'
 import { FindPostsByUser } from '../../../core/usecases/posts/FindPostsByUser'
+import { CustomRequest } from '../../http/middlewares/AuthMiddleware'
 
 export class FindPostsByUserController {
     constructor (
         readonly findPostsByUser: FindPostsByUser
     ) { }
     
-    async handle(req: Request, res: Response, next: NextFunction): Promise<Response> {
+    async handle(req: CustomRequest, res: Response, next: NextFunction): Promise<Response> {
         try {
             const input = {
                 id: req.params.id,
-                token: req.body.token
+                token: req.token
             }
             const posts = await this.findPostsByUser.execute(input)
             return res.status(200).json(posts)
